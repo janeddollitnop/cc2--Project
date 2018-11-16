4c285703d2338ae83c6d91fa59e270fd2e923562
janeddollitnop <45084983+janeddollitnop@users.noreply.github.com>
2018-11-16 11:08:40 +0800

    Add files via upload

---
+++ FibArray.java
@@ -0,0 +1,21 @@
+package Fib;
+public class FibArray {
+    public static void main(String[] args) {
+        int[] array = {3,5,7,13,2,3,9};
+        display(array);
+    }
+    static void display(int [] ar){
+        for (int j = 0; j < ar.length; j++){
+            System.out.println("f(" + ar[j] + "):\t"+ fibonacci(ar[j]));
+            }
+        }
+    static int fibonacci(int nth){
+        if (nth <= 1){
+        return nth;
+        }
+        else{
+        return fibonacci(nth - 1) + fibonacci(nth - 2);    
+        }
+    }
+}
+
